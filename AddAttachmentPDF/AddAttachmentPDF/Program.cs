505e18a1118505614f3212ad4fe6b77302330a4e
sameerkhan001 <sameerkhan.n@syncfusion.com>
2023-05-08 16:44:40 +0530

    828251 Added sample project and Readme file in attachment blog.

---
+++ AddAttachmentPDF/AddAttachmentPDF/Program.cs
@@ -0,0 +1,25 @@
+﻿using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf.Parsing;
+
+//Load the PDF document
+FileStream docStream = new FileStream("../../../Input.pdf", FileMode.Open, FileAccess.Read);
+PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
+
+//Creates an attachment
+Stream fileStream = new FileStream("../../../Input.txt", FileMode.Open, FileAccess.Read);
+PdfAttachment attachment = new PdfAttachment("Input.txt", fileStream);
+attachment.ModificationDate = DateTime.Now;
+attachment.Description = "Input.txt";
+attachment.MimeType = "application/txt";
+
+if (loadedDocument.Attachments == null)
+    loadedDocument.CreateAttachment();
+//Add the attachment to the document
+loadedDocument.Attachments.Add(attachment);
+
+//Save the document into stream
+MemoryStream stream = new MemoryStream();
+loadedDocument.Save(stream);
+File.WriteAllBytes("../../../Output.pdf",stream.ToArray());
+loadedDocument.Close(true);
+stream.Close();
---
+++ ExtractAttachmentPDF/ExtractAttachmentPDF/Program.cs
@@ -0,0 +1,22 @@
+﻿//Load the PDF document
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf.Parsing;
+using System.IO;
+
+//Load the PDF document
+FileStream docStream = new FileStream("../../../Input.pdf", FileMode.Open, FileAccess.Read);
+//Load an existing PDF document 
+PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
+
+//Iterates the attachments 
+foreach (PdfAttachment attachment in loadedDocument.Attachments)
+{
+    //Extracts the attachment and saves it to the disk 
+    FileStream s = new FileStream(attachment.FileName, FileMode.Create);
+    s.Write(attachment.Data, 0, attachment.Data.Length);
+    s.Dispose();
+}
+
+//Close the PDF document. 
+loadedDocument.Close(true);
